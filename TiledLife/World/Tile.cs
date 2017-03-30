435618b258517c3ef03fd1a571448789548156c4
Knuk <samuellapointe@gmail.com>
2017-03-30 19:04:05 -0400

    Move tile generation to a seperate class

--- TiledLife/World/Tile.cs
+++ TiledLife/World/Tile.cs
@@ -4,8 +4,6 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using TiledLife.Creature;
-using TiledLife.World.Materials.Liquid;
-using TiledLife.World.Materials.Solid;
 
 namespace TiledLife.World
 {
@@ -47,39 +45,16 @@ namespace TiledLife.World
             
             random = new Random();
 
-            // Create blocks
-            blocks = new Block[TILE_HEIGHT, TILE_WIDTH];
-
         }
 
         public void Initialize()
         {
             for (int i = 0; i < 10; i++)
             {
-                creatures.Add(new Sheep(new Vector2(100 + i * 10, 500)));
+                //creatures.Add(new Sheep(new Vector2(100 + i * 10, 500)));
             }
 
-            for (int i = 0; i < TILE_HEIGHT; i++)
-            {
-                for (int j = 0; j < TILE_WIDTH; j++)
-                {
-                    int randInt = random.Next(10);
-                    if (random.Next(10) == 0)
-                    {
-                        blocks[i, j] = new Block(
-                                random,
-                                MaterialLiquid.CreateMaterial(MaterialLiquid.MaterialLiquidNames.water),
-                                j, i
-                            );
-                    } else {
-                        blocks[i, j] = new Block(
-                                random,
-                                MaterialSolid.CreateMaterial(MaterialSolid.MaterialSolidNames.dirt),
-                                j, i
-                            );
-                    }
-                }
-            }
+            blocks = TileGenerator.GenerateTile(random, TILE_HEIGHT, TILE_WIDTH);
         }
 
         public void LoadContent(ContentManager content)
@@ -108,21 +83,6 @@ namespace TiledLife.World
 
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
-            /*if (texture == null)
-            {
-                texture = new Texture2D(spriteBatch.GraphicsDevice, imageWidth, imageHeight);
-                int nbPixels = imageWidth * imageHeight;
-                Color[] colorData = new Color[nbPixels];
-
-                for (int i = 0; i < nbPixels; i++)
-                {
-                    colorData[i] = new Color(random.Next(50, 60), random.Next(30, 40), 0);
-                }
-                texture.SetData<Color>(colorData);
-            }
-            Vector2 offset = new Vector2(TILE_WIDTH * tileX, TILE_HEIGHT * tileY);
-            spriteBatch.Draw(texture, offset);*/
-
             int offsetX = TILE_WIDTH * tileX;
             int offsetY = TILE_HEIGHT * tileY;
             foreach (Block block in blocks)
---
+++ TiledLife/World/TileGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TiledLife.World.Materials.Liquid;
+using TiledLife.World.Materials.Solid;
+
+namespace TiledLife.World
+{
+    static class TileGenerator
+    {
+        public static Block[,] GenerateTile(Random random, int tileHeight, int tileWidth)
+        {
+            Block[,] blocks = new Block[tileHeight, tileWidth];
+            for (int i = 0; i < tileHeight; i++)
+            {
+                for (int j = 0; j < tileWidth; j++)
+                {
+                    if (random.Next(20) == 0)
+                    {
+                        blocks[i, j] = new Block(
+                                random,
+                                MaterialLiquid.CreateMaterial(MaterialLiquid.MaterialLiquidNames.water),
+                                j, i
+                            );
+                    }
+                    else
+                    {
+                        blocks[i, j] = new Block(
+                                random,
+                                MaterialSolid.CreateMaterial(MaterialSolid.MaterialSolidNames.dirt),
+                                j, i
+                            );
+                    }
+                }
+            }
+
+            return blocks;
+        }
+    }
+}
